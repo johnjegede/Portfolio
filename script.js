18b56e9860a5518d6882a49b38aa81f2d88547dc
John Jegede <jegede400@gmail.com>
2021-01-20 15:43:16 -0500

    added images and some functions

--- script.js
+++ script.js
@@ -4,6 +4,55 @@ $(document).ready(function(){
     var $tab = $($navItems).find("li.active");
     var $link = $tab.find('a');
     var $panel = $($link.attr('href'));
+    var content;
+
+    $navItems.on("mouseover",function(e){
+
+        var currlink = e.target;
+        var id = currlink.getAttribute('href');
+        
+
+        if(id == "#about")
+        {
+            content = e.target.textContent;
+            console.log(e.target.textContent);
+            e.target.textContent = "About";
+        }else if(id == "#skills")
+        {
+            e.target.textContent = "Skills";
+        }else if(id == "#portfolio")
+        {
+            e.target.textContent = "Portfolio";
+        }else if(id == "#contact")
+        {
+            e.target.textContent = "Contact";
+        }
+
+     });
+
+     $navItems.on("mousemove",function(e){
+
+        var currlink = e.target;
+        var id = currlink.getAttribute('href');
+        
+
+        if(id == "#about")
+        {
+            //content = e.target.textContent;
+            e.target.textContent = content;
+        }else if(id == "#skills")
+        {
+            //e.target.textContent = "Skills";
+        }else if(id == "#portfolio")
+        {
+            //e.target.textContent = "Portfolio";
+        }else if(id == "#contact")
+        {
+            //e.target.textContent = "Contact";
+        }
+
+     });
+
 
     $navItems.on("click",function(e){
         e.preventDefault();
